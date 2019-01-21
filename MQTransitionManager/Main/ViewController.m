a65a7f98efa9f6ccb04098df8bf5d410a4454f47
M_noAria <46508354+M-Quadra@users.noreply.github.com>
2019-01-21 15:44:01 +0800

    add demo

--- MQTransitionManager/Main/ViewController.m
+++ MQTransitionManager/Main/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "MQTransitionManager.h"
 
 @interface ViewController ()
 
@@ -17,7 +18,37 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor lightGrayColor];
+    self.navigationItem.title = [@(self.navigationController.viewControllers.count) stringValue];
+    
+    UIButton *btnLft = [[UIButton alloc] initWithFrame:CGRectMake(0, 100, 50, 50)];
+    btnLft.backgroundColor = [UIColor redColor];
+    [btnLft addTarget:self action:@selector(lftBtnClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btnLft];
+    
+    UIButton *btnRit = [[UIButton alloc] initWithFrame:CGRectMake(0, 150, 50, 50)];
+    btnRit.backgroundColor = [UIColor greenColor];
+    [btnRit addTarget:self action:@selector(ritBtnClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btnRit];
 }
 
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    MQTransitionManager *transition = [MQTransitionManager shareManagerWithOperation:UINavigationControllerOperationPop viewController:self];
+    [transition setPopType:self.navigationController.viewControllers.count&1 ? MQTransitionType_Normal : MQTransitionType_CoverVertical];
+}
 
+- (void)lftBtnClick:(UIButton *)sender {
+    ViewController *vc = [[ViewController alloc] init];
+    
+    MQTransitionManager *transition = [MQTransitionManager shareManagerWithOperation:UINavigationControllerOperationPush viewController:vc];
+    [transition pushWithType:MQTransitionType_Normal navigationController:self.navigationController];
+}
+
+- (void)ritBtnClick:(UIButton *)sender {
+    ViewController *vc = [[ViewController alloc] init];
+    
+    MQTransitionManager *transition = [MQTransitionManager shareManagerWithOperation:UINavigationControllerOperationPush viewController:vc];
+    [transition pushWithType:MQTransitionType_CoverVertical navigationController:self.navigationController];
+}
 @end
